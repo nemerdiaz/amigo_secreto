3816c1bbf73f2b107a84ce2a84d44a8669687f6e
unknown <nemerdiaz@gmail.com>
2025-03-06 00:11:50 -0300

    Juego terminado

--- app.js
+++ app.js
@@ -3,25 +3,46 @@
 let amigos = [];
 
 function agregarAmigo(){
-
-    let amigo = document.querySelector('.input-name').value;
-    // console.log(amigo);
+    let amigo = document.querySelector('.input-name').value;    
     if (amigo != "") {
-        if (typeof amigo == "string") {
-            amigos.push(amigo);
-            // console.log(amigos);
-            limpiarCaja();
-                return amigo;   
-        }
-
+        amigos.push(amigo);
+        // console.log(amigos);
+        limpiarCaja();
+        recorrerAmigos()
+        return amigo;                    
     } else {
-        alert("Por favor, inserte un nombre.");
-        
+        alert("Por favor, inserte un nombre.");        
     }    
-    
-
 }
 
 function limpiarCaja() {
     document.getElementById('amigo').value = '';
 }
+
+function sortearAmigo() {
+    if (amigos.length===0) {
+        alert("Antes del sorteo, debe ingresar nombre."); 
+    }else{    
+    let amigoElegido = Math.floor(Math.random() * amigos.length);
+    document.getElementById("listaAmigos").innerHTML = "";
+    document.getElementById("listaAmigos").innerHTML = "El amigo secreto es: " + amigos[amigoElegido];
+    // console.log(amigos);
+    // console.log(amigoElegido);
+    // console.log(amigos[amigoElegido]);
+    return amigos[amigoElegido];
+    }
+}
+
+function recorrerAmigos() {     
+    document.getElementById("listaAmigos").innerHTML = ""; 
+    for (i = 0; i < amigos.length; i++) {
+        let listar = document.createElement("li");
+        listaAmigos = amigos[i];      
+        listar.appendChild(document.createTextNode(listaAmigos));      
+        document.querySelector("#listaAmigos").appendChild(listar);
+    }    
+}
+
+
+
+
